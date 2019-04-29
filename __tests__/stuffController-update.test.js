0136bb419234906f1208562c6d7fb485d78433ac
Shanshan Chen <shanshanc.chen@gmail.com>
2019-04-29 15:46:12 +0200

    modify the controller update test

--- __tests__/stuffController-update.test.js
+++ __tests__/stuffController-update.test.js
@@ -3,7 +3,7 @@ const stuffController = require('../controllers/stuffControllers');
 jest.mock('../db/stuffModel');
 
 describe('test stuffController update function', () => {
-  it('update if finds an id', async () => {
+  it('update if id exists', async () => {
     const ctx = {
       request: {
         method: 'GET',
@@ -31,8 +31,37 @@ describe('test stuffController update function', () => {
         picture: 'https://example.com/pic2'
       }
     ];
-
     Stuff.findOneAndUpdate.mockImplementation(() => fakeDB[0]);
+    await stuffController.update(ctx, () => {});
+    console.log(ctx);
+    expect(ctx.status).toEqual(204);
+  });
+  
+  it('create new if id is not found', async () => {
+    const ctx = {
+      request: {
+        method: 'GET',
+        header: {
+          'Content-Type': 'application/json'
+        },
+        body: {
+          picture: 'https://example.com/pic1/newlink',
+          time: '2018-08-01T10:51:57.108Z',
+          tags: ['tag1', 'tag2']
+        }
+      },
+      params: {
+        id: 'abcd'
+      }
+    };
+    const fakeNewRecord = {
+      id: 'abcd',
+      picture: 'https://example.com/pic3',
+      time: '2018-08-01T10:51:57.108Z',
+      tags: ['tag1', 'tag2']
+    };
+
+    Stuff.findOneAndUpdate.mockImplementation(() => fakeNewRecord);
     await stuffController.update(ctx, () => {});
     expect(ctx.status).toEqual(204);
   });
