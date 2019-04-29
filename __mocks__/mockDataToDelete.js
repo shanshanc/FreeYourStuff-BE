5471ee5d64aa6c6b5a929ea7e0315091d1a57937
Shanshan Chen <shanshanc.chen@gmail.com>
2019-04-29 20:08:44 +0200

    modify delete test and mock data

---
+++ __mocks__/mockDataToDelete.js
@@ -0,0 +1,11 @@
+module.exports.data = {
+  request: {
+    method: 'DELETE',
+    header: {
+      'Content-Type': 'application/json'
+    }
+  },
+  params: {
+    id: '1234'
+  }
+};
--- __tests__/stuffController-delete.test.js
+++ __tests__/stuffController-delete.test.js
@@ -1,36 +1,17 @@
 const Stuff = require('../db/stuffModel');
 const stuffControllers = require('../controllers/stuffControllers');
+const data = require('../__mocks__/mockDataToDelete');
 
 jest.mock('../db/stuffModel');
 
 describe('controller: delete', () => {
   test('should respond as expected', async () => {
-    const ctx = {
-      request: {
-        method: 'DELETE',
-        header: {
-          'Content-Type': 'application/json'
-        }
-      },
-      params: {
-        id: '1234'
-      }
-    };
+    const ctx = Object.values(data)[0];
 
-    const fakeDB = [
-      {
-        _id: '1234',
-        picture: 'https://example.com/pic1'
-      },
-      {
-        _id: '5678',
-        picture: 'https://example.com/pic2'
-      }
-    ];
-    Stuff.prototype.remove.mockImplementation(() => {});
-    await stuffControllers.delete(ctx, () => { });
-    
-    expect(ctx.status === 204);
+    Stuff.prototype.remove.mockImplementation((args) => jest.fn());
+    await stuffControllers.delete(ctx, () => {});
     expect(ctx.status).toEqual(204);
+    expect(Stuff.remove).toHaveBeenCalledTimes(1);
+    expect(Stuff.remove).toHaveBeenCalledWith({_id: ctx.params.id});
   });
 });
